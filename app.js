901956de0ffb801f32c72262a76126d8b26e5528
Racheal Gathagu <rachealkari@gmail.com>
2022-02-14 09:53:33 +0300

    connecting the database to our application

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 // Importing express package
 const express = require('express');
+const mongoose = require('mongoose');
 
 // Initializing express
 const app = express();
 
 // Setting a static folder
 app.use(express.static('public'));
+
+// Connecting to the Database
+let mongodb_url = 'mongodb://localhost/';
+let dbName = 'contact_manager';
+mongoose.connect(mongodb_url + dbName,)
+let db = mongoose.connection;
+
+// Check Connection
+db.once('open', ()=>{
+   console.log('Database connected successfully')
+})
+
+// Check for DB Errors
+db.on('error', (error)=>{
+   console.log(error);
+})
 
 // Defining the port number
 const PORT = 5000;
