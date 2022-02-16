f2390f0639b340e7c89eb83e604e98aca2d113e5
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-02-16 18:01:42 +0300

    connected edit contact view to database

--- routes/edit_route.js
+++ routes/edit_route.js
@@ -7,9 +7,14 @@ const contacts = require('../models/view_models');
 
   //route to handle editing of contacts
   router.get('/',(req,res)=>{
-   
-    res.render("editcontact")
-
+    contacts.find(req.query, (error, contact)=>{
+        if (error){
+            console.log(error);
+        }else{
+            console.log(contact)
+            res.render("editcontact", { contact:contact[0] })
+        }
+    })
 })
 
 module.exports = router;
