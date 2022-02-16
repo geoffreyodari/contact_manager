424220737f4d66ad9eceff0d69e3eb32ff0e9523
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-02-16 20:08:23 +0300

    updated data validation

--- public/js/main.js
+++ public/js/main.js
@@ -5,17 +5,23 @@ $(document).ready(()=>{
         let phoneExpression = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
         let emailExpression = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
-        if($(':input').val()==''){
+        if($('#othernames').val() ==''){
             e.preventDefault()
-            $('.error-msg').text('inputs cannot be blank')
-        }else if(!emailExpression.test($('#email').val())){
+            $('#othernames').siblings('p').text('Other names cannot be blank')
+        }
+
+        if($('#firstname').val()==''){
             e.preventDefault()
-            $('#email').siblings('p').text('please enter a valid email')
-        }else if(!phoneExpression.test($('#phone').val())){
+            $('#firstname').siblings('p').text('First name cannot be blank')
+        }
+
+        if(!emailExpression.test($('#email').val())){
             e.preventDefault()
-            $('#email').siblings('p').text('please enter a valid 10 digit phone number')
-        }else{
-            console.log("OK")
+            $('#email').siblings('p').text('Please enter a valid email')
+        } 
+        if(!phoneExpression.test($('#phone').val())){
+            e.preventDefault()
+            $('#phone').siblings('p').text('Please enter a valid 10 digit phone number')
         }
     })
 })
