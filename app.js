84dac59c6a70ec03524d44fa126c4baeac2d6930
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-02-25 12:50:28 +0300

    configured heroku and  atlas

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const mongoose = require('mongoose');
 
 let indexRouter = require('./routes/index');
 
-let mongodbUrl = 'mongodb+srv://odari:hPhMqq4BzVuSrksZ@cluster0.7wdrl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+let mongodbUrl = 'mongodb+srv://odari:hPhMqq4BzVuSrksZ@cluster0.7wdrl.mongodb.net/contact_manager?retryWrites=true&w=majority';
 let dbName = 'contact_manager';
 mongoose.connect(mongodbUrl+dbName)
 let db = mongoose.connection;
